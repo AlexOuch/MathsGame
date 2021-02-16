685a408e5dc687cee7de20077b75f1f826e531c4
AlexOuch <6285@ait.nsw.edu.au>
2021-02-17 10:57:11 +1100

    moved project into git folder

---
+++ Assets/player.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+    public Canvas canvas;
+    public Camera camera;
+    // Start is called before the first frame update
+    void Start()
+    {
+        canvas.transform.Rotate(180, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 v = camera.transform.position - transform.position;
+        v.x = v.z = 0.0f;
+        canvas.transform.LookAt(camera.transform.position - v);
+        canvas.transform.Rotate(0, 180, 0);
+    }
+}
