a75e41d29a12da76e1d5227e00351fca1952488c
AlexOuch <38543937+AlexOuch@users.noreply.github.com>
2021-02-24 17:05:29 +1100

    fixed questions, started menu

---
+++ Assets/Scripts/Button.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Button : MonoBehaviour
+{
+    public GameObject player;
+    public Text text;
+    public GameObject canvasSuccess;
+    public GameObject canvasFail;
+
+    public Transform mainCanvas; //main canvas
+
+    void Start()
+    {
+        mainCanvas.GetComponent<Question>().getQuestion();
+    }
+    
+    /*public void Question()
+    {        
+        mainCanvas.GetComponent<Question>().getQuestion();
+    }
+    */
+
+    public void Answer()
+    {
+        
+        double answer = mainCanvas.GetComponent<Question>().getAnswer();
+        print(answer.ToString(".00"));
+        if (text.text == answer.ToString(".00"))
+        {
+            canvasSuccess.transform.gameObject.SetActive(true);
+            print("correct!");
+            StartCoroutine(TwoSecondsSuccess());
+                        
+        }
+        else
+        {
+            canvasFail.transform.gameObject.SetActive(true);
+            print("fail :(");
+            StartCoroutine(TwoSecondsFail());
+        }
+
+        //mainCanvas.gameObject.SetActive(false);
+            
+    }
+
+    IEnumerator TwoSecondsSuccess()
+    {
+        yield return new WaitForSeconds(1f);
+        canvasSuccess.transform.gameObject.SetActive(false);
+        mainCanvas.GetComponent<Question>().getQuestion();
+    }
+
+    IEnumerator TwoSecondsFail()
+    {
+        yield return new WaitForSeconds(1f);
+        canvasFail.transform.gameObject.SetActive(false);
+        mainCanvas.GetComponent<Question>().getQuestion();
+    }
+
+    
+}
+
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,22 +2,30 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class player : MonoBehaviour
+public class Player : MonoBehaviour
 {
     public Canvas canvas;
     public Camera camera;
     // Start is called before the first frame update
     void Start()
     {
-        canvas.transform.Rotate(180, 0, 0);
+        //canvas.transform.Rotate(180, 0, 0);
     }
 
     // Update is called once per frame
     void Update()
     {
+        /*
         Vector3 v = camera.transform.position - transform.position;
         v.x = v.z = 0.0f;
         canvas.transform.LookAt(camera.transform.position - v);
         canvas.transform.Rotate(0, 180, 0);
+        */
+    }
+
+    public IEnumerator CorrectAnswer()
+    {
+        yield return new WaitForSeconds(2);
+
     }
 }
